706f604bf3538e01263b3d9bf8157d9854ab245f
Vova-Kalbenko <lastforhs3@gmail.com>
2023-05-22 12:46:25 +0300

    fix-8-task-js

--- js/task-08.js
+++ js/task-08.js
@@ -4,13 +4,16 @@ const passwordInputEl = document.querySelector('[name="password"]')
 
 
 const handleSubmitButton = (e) =>{
+    const userEmailAndPassword = {
+        email: 'emailInputEl.value' ,
+        password: 'passwordInputEl.value'
+    }
     e.preventDefault()
-    
     if(emailInputEl.value === '' || passwordInputEl.value === ''){
         window.alert(`all fields must be filled in.`)
         return
     }
-    console.log(`{email: ${emailInputEl.value}, password: ${passwordInputEl.value}}`)
+    console.log(userEmailAndPassword)
 }
 
 InputEl.addEventListener('submit', handleSubmitButton)
